e8b090dbb825d6529ac88def26a64a7ca275521d
jganseman <joachim.ganseman@student.ua.ac.be>
2016-02-05 10:53:05 +0100

    add new author

--- src/MarcRecord.cpp
+++ src/MarcRecord.cpp
@@ -635,7 +635,7 @@ bool MarcRecord::isCRB() const
             || (author == "df") || (author == "dl") || (author == "ed") || (author == "fb") || (author == "hl")
             || (author == "id") || (author == "ls") || (author == "mdd") || (author == "mt") || (author == "md")
             || (author == "rs") || (author == "th") || (author == "vdf") || (author == "mm") || (author == "hch")
-            || (author == "yi"))
+            || (author == "yi") || (author == "smv"))
         {
             return true;
         } else
